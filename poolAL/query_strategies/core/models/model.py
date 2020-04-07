cfa3c175fad076a272c0ac589952c9bca43614ce
SimiPixel <Simon.Bachhuberr@gmail.com>
2020-04-07 14:04:49 +0200

    predict_proba no longer abstract method

--- poolAL/query_strategies/core/models/model.py
+++ poolAL/query_strategies/core/models/model.py
@@ -18,28 +18,28 @@ class Model(ABC):
     def train(self, dataset, *args, **kwargs):
         '''
         Train a model given a dataset
-        
+
         Parameters
         ----------
         dataset: Dataset object
         '''
         pass
-    
+
     @abstractmethod
     def predict(self, feature, *args, **kwargs):
         '''
         Predict the class label of input
-        
+
         Parameters
         ----------
         feature: np array, shape = (n_samples, n_features)
-        
+
         Returns
         -------
         y_pred: np array, shape = (n_samples)
         '''
         pass
-    
+
     @abstractmethod
     def score(self, testing_dataset, *args, **kwargs):
         """Return the mean accuracy on the test dataset
@@ -54,7 +54,8 @@ class Model(ABC):
             Mean accuracy of self.predict(X) wrt. y.
         """
         pass
-    
+
+'''
     @abstractmethod
     def predict_proba(self, feature, *args, **kwargs):
         """Predict probability estimate for samples.
@@ -68,10 +69,6 @@ class Model(ABC):
             Each entry is the prabablity estimate for each class.
         """
         pass
-
+'''
 
 # In[ ]:
-
-
-
-
