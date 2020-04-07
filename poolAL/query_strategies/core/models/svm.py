a9d0420da3773d61f5edd56ce866eafae2f89314
SimiPixel <Simon.Bachhuberr@gmail.com>
2020-04-07 13:11:45 +0200

    *Added support for probability = False

--- poolAL/query_strategies/core/models/svm.py
+++ poolAL/query_strategies/core/models/svm.py
@@ -12,27 +12,25 @@ from sklearn.svm import SVC
 
 
 class SVM(Model):
-    
+    '''
+    SVM with default probability = 1
+    '''
     def __init__(self, *args, **kwargs):
-        self.model = SVC(probability = True, *args, **kwargs)
-        
+        self.model = SVC(probability = kwargs.pop('probability', True), *args, **kwargs)
+
     def train(self, dataset):
         X, y = dataset.get_labeled_entries()
         self.model.fit(X, y)
-        
+
     def predict(self, feature):
         return self.model.predict(feature)
-    
+
     def predict_proba(self, feature):
         return self.model.predict_proba(feature)
-    
+
     def score(self, dataset):
         X, y = dataset.get_labeled_entries()
         return self.model.score(X, y)
 
 
 # In[ ]:
-
-
-
-
