7fa7c24a384569ba2b7c90c786bf5996a68b39d9
SimiPixel <Simon.Bachhuberr@gmail.com>
2020-04-24 16:49:54 +0200

    implemented utils.shuffle, added warm_start=1 support

--- poolAL/evaluate/calc_score.py
+++ poolAL/evaluate/calc_score.py
@@ -1,11 +1,11 @@
 import copy
-from sklearn.utils import shuffle
+from ..query_strategies.core.utils import shuffle
 from ..query_strategies.core import Dataset
 import numpy as np
 import time
 from IPython.display import clear_output
 
-def CalcScore(_X, _y, qs, qs_kwargs, clf, n_labels_start, n_labels_end, n_runs, n_unique_labels_start, **kwargs):
+def CalcScore(_X, _y, qs, qs_kwargs, clf, clf_kwargs, n_labels_start, n_labels_end, n_runs, n_unique_labels_start, **kwargs):
     '''
     Calculates the mean score of a list of query strategies (or a single qs) given a budget of labels
 
@@ -15,26 +15,40 @@ def CalcScore(_X, _y, qs, qs_kwargs, clf, n_labels_start, n_labels_end, n_runs, 
 
     X: {np.array} of shape (n_samples, n_features)
         Input data
+
     y: {list}
         labels
+
     qs: {list} of poolAL.query_strategies.core.query_strategy.QueryStrategy objects
         List of query strategies that should be used
         NOTE: If a committee QueryStrategy is included in qs, then the repspective kwargs dictionary in
         qs_kwargs must include key 'query_strategy'. Its value must be a tuple of the form:
         ({list} of QueryStrategy objects, {list} of kwargs-Dictionaries)
         For example see bottom.
+
     qs_kwargs: {list} of Dictionaries
         Each query strategies repspective kwargs
-    clf: {poolAL.query_strategies.core.models.model.Model}
-        The classifier used to calculate the test scores
+
+    clf: {list} of poolAL.query_strategies.core.models.model.Model objects
+        The classifier for every query strategy used to calculate the test scores
+
+    clf_kwargs: {list} of Dictionaries
+        Each classfiers repspective kwargs
+
     n_labels_start: {int}
         Number of initially available labels
+
     n_labels_end: {int}
         Number of labels when finished
+
     n_runs: {int}
         Number of runs for averaging
+
     n_unique_labels_start: {int}
         Number of unique labels in initial labels
+
+    random_state: {int}
+        default = None
 
 
     Returns
@@ -52,6 +66,8 @@ def CalcScore(_X, _y, qs, qs_kwargs, clf, n_labels_start, n_labels_end, n_runs, 
     X, y = load_iris(return_X_y=True)
     y = y.tolist()
     clf = SVM(kernel = 'linear', random_state = 1)
+    clf_test = 4*[SVM]
+    clf_test_kwargs = 4*[{'kernel':'linear', 'random_state': 1}]
 
     deal_qs = [ClusterMarginSampling, UncertaintySampling]
     deal_qs_kwargs = [{'space':'full'}, {'model': clf}]
@@ -59,13 +75,13 @@ def CalcScore(_X, _y, qs, qs_kwargs, clf, n_labels_start, n_labels_end, n_runs, 
     qs = [RandomSampling, ClusterMarginSampling, UncertaintySampling, DynamicEnsembleActiveLearning]
     qs_kwargs = [{}, {'space':'full'}, {'model': clf}, {'query_strategy': (deal_qs, deal_qs_kwargs), 'model': clf, 'T':97}]
 
-    CalcScore(X, y, qs, qs_kwargs, clf, 3, 100, 200, 3)
+    CalcScore(X, y, qs, qs_kwargs, clf_test, clf_test_kwargs, 3, 100, 200, 3)
 
     '''
 
     # Optionally fixing the random seed to make it perfectly reproducable
     np.random.seed(kwargs.pop('random_state', None))
-    random_state = np.array([np.random.randint(0, 2**32, size = 100) for x in range(n_runs)])
+    random_state = np.array([np.random.randint(0, 2**32) for x in range(n_runs)])
 
 
     # Number of query strategies
@@ -91,31 +107,30 @@ def CalcScore(_X, _y, qs, qs_kwargs, clf, n_labels_start, n_labels_end, n_runs, 
             print('Progress:',round((i+1)/n_runs*100,3),'%')
             print('Finished in approximately',(t2/60/i)*(n_runs-i),'minutes.')
 
-        # Shuffle until every class is present in initial data
-        escape = -1
-        while True:
-            escape += 1
-            X, y = shuffle(_X, _y, random_state = random_state[i, escape])
-            if len(set(y[:n_labels_start])) >= n_unique_labels_start:
-                # Found a permutation
-                break
-            # Raise Error if its too hard
-            if escape > 99:
-                raise Exception('Ensuring that enough classes are present initially is too hard')
+        # Shuffle
+        X, y = shuffle(_X, _y, n_unique_labels_start, random_state[i])
 
         # Create Datasets
         data_train = Dataset(X[:n_labels_end], y[:n_labels_start]+(n_labels_end-n_labels_start)*[None])
+
         # Test pool is not larger than 300 samples
         data_test = Dataset(X[n_labels_end:n_labels_end+300], y[n_labels_end:n_labels_end+300])
 
         # Create copies of trainings data
         data_train = [copy.deepcopy(data_train) for i in range(N)]
 
-        # Instantiate query strategies
+
+        # Instantiate query strategies and classifiers
         qs_inst = []
+        clf_inst = []
+
         for j in range(N):
 
-            # Instantiate the query_strategy list for Committee's
+            # Instantiate the classifier to calculate the test scores
+            # for every query strategy
+            clf_inst.append(clf[j](**clf_kwargs[j]))
+
+            # Instantiate the query_strategy list for Committee's / Bandit's
             if 'query_strategy' in qs_kwargs[j]:
                 temp_kwargs = copy.deepcopy(qs_kwargs[j])
                 tup = temp_kwargs.pop('query_strategy')
@@ -130,8 +145,8 @@ def CalcScore(_X, _y, qs, qs_kwargs, clf, n_labels_start, n_labels_end, n_runs, 
         for j in range(n_labels):
             for k in range(N):
                 # Score
-                clf.train(data_train[k])
-                result[k,j,1] += clf.score(data_test)
+                clf_inst[k].train(data_train[k])
+                result[k,j,1] += clf_inst[k].score(data_test)
                 if i == 0:
                     result[k,j,0] = j + n_labels_start
 
