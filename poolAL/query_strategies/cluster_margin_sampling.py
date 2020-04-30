9fc4d42006bdbbcbe2ea2a79dc1bce49f3d52e6d
SimiPixel <Simon.Bachhuberr@gmail.com>
2020-04-30 15:21:47 +0200

    added confidence_grid

--- poolAL/query_strategies/cluster_margin_sampling.py
+++ poolAL/query_strategies/cluster_margin_sampling.py
@@ -38,17 +38,17 @@ class ClusterMarginSampling(QueryStrategy):
         self.kMeans = KMeans(n_clusters = self.nr_of_clusters, random_state= 2)
         self.kMeans.fit(self.dataset._X)
 
-    def make_query(self, size = 1):
-
+    def _get_scores(self):
         if self.space == 'labeled':
             X = self.dataset.get_labeled_entries()[0]
         elif self.space == 'full':
             X = self.dataset.get_entries()[0]
 
         # Check if number of clusters has changed and fit if yes
-        if self.dataset.get_num_of_labels() != self.nr_of_clusters:
+        if self.dataset.get_num_of_labels() != self.nr_of_clusters or self.dataset.modified_X:
+            self.dataset.modified_X = False
             self.nr_of_clusters = self.dataset.get_num_of_labels()
-            self.kMeans = KMeans(n_clusters = self.nr_of_clusters)
+            self.kMeans = KMeans(n_clusters = self.nr_of_clusters, random_state= 2)
             self.kMeans.fit(X)
 
         # Otherwise only fit if space is 'labeled'
@@ -66,10 +66,34 @@ class ClusterMarginSampling(QueryStrategy):
         X_sorted = np.array([e[1]-e[0] for e in X])
         result = zipit(X_ids, X_sorted)
 
+        return result
+
+
+    def make_query(self, size = 1):
+        result = self._get_scores()
         # Sort by 2nd, the lower the better
         result = sort_by_2nd(result, 'min')
 
         return result[:size, 0].astype(int)
 
     def confidence(self):
-        pass
+        '''
+        The lower the better
+        '''
+
+        result = self._get_scores()
+
+        return result[:,1]
+
+    def confidence_grid(self, grid_X):
+        '''
+        The lower the better
+        '''
+
+        # This just fits the kMean alg
+        _ = self._get_scores()
+
+        X = self.kMeans.transform(grid_X)
+
+        X.sort()
+        return np.array([e[1]-e[0] for e in X])
